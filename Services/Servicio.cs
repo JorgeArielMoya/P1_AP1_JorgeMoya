5895deb15f0b7dc2bc5127f139e88e4bf21e1de2
jorge <jorge@LAPTOP-D8GDLQRL>
2025-09-22 20:12:32 -0400

    Se creo el servicio

---
+++ Services/Servicio.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP1_JorgeMoya.DAL;
+
+namespace P1_AP1_JorgeMoya.Services
+{
+    public class Servicio(IDbContextFactory<Contexto>DbFactory)
+    {
+
+    }
+}
