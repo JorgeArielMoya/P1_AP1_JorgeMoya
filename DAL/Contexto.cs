9a62449bac17cbce34c412ecba032a64baba4455
jorge <jorge@LAPTOP-D8GDLQRL>
2025-09-22 20:08:03 -0400

    Se creo el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP1_JorgeMoya.Models;
+
+namespace P1_AP1_JorgeMoya.DAL;
+
+public class Contexto : DbContext
+{
+    public DbSet<Registro> Registros { get; set; }
+
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+}
