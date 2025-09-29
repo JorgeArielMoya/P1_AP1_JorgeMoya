81af4ef03ed7e8e02b0ed0d094c8a3d1c7eaac12
jorge <jorge@LAPTOP-D8GDLQRL>
2025-09-29 19:26:48 -0400

    Otra migracion por errores y validaciones

---
+++ Migrations/20250929231212_TableEntradas.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace P1_AP1_JorgeMoya.Migrations
+{
+    /// <inheritdoc />
+    public partial class TableEntradas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Registros",
+                table: "Registros");
+
+            migrationBuilder.RenameTable(
+                name: "Registros",
+                newName: "EntradasHuacales");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_EntradasHuacales",
+                table: "EntradasHuacales",
+                column: "IdEntrada");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_EntradasHuacales",
+                table: "EntradasHuacales");
+
+            migrationBuilder.RenameTable(
+                name: "EntradasHuacales",
+                newName: "Registros");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Registros",
+                table: "Registros",
+                column: "IdEntrada");
+        }
+    }
+}
