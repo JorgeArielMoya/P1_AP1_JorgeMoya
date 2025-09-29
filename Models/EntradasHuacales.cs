52a75778022f5eca889ca23c72241adca9d483ac
jorge <jorge@LAPTOP-D8GDLQRL>
2025-09-29 18:18:44 -0400

    Se agregaron los campos

--- Models/EntradasHuacales.cs
+++ Models/EntradasHuacales.cs
@@ -5,5 +5,17 @@ namespace P1_AP1_JorgeMoya.Models;
 public class EntradasHuacales
 {
     [Key]
-    public int Id { get; set; }     
+    public int IdEntrada { get; set; }
+
+    [Required(ErrorMessage = "Campo Requerido")]
+    public string NombreCliente { get; set; } = null!;
+
+    [Required(ErrorMessage = "Campo Requerido")]
+    [Range(0, int.MaxValue, ErrorMessage = "Cantidad no valida")]
+    public int Cantidad {  get; set; }
+
+    [Range(0.01, double.MaxValue, ErrorMessage = "Precio no valido")]
+    public double Precio { get; set; }
+
+    public DateTime Fecha { get; set; } = DateTime.Now;
 }
