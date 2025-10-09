76a5a83772448506175c7a62e862fa2674c51ad6
jorge <jorge@LAPTOP-D8GDLQRL>
2025-10-08 21:12:49 -0400

    Se trabajo el modelo tipoHuacales

---
+++ Models/TiposHuacales.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P1_AP1_JorgeMoya.Models;
+
+public class TiposHuacales
+{
+    [Key]
+    public int TipoId { get; set; }
+
+    [Required(ErrorMessage = "Campo requerido")]
+    public string Descripcion { get; set; } = null!;
+
+    [Required(ErrorMessage = "Campo requerido")]
+    [Range(0, int.MaxValue, ErrorMessage = "Existencia no valida")]
+    public int Existencia { get; set; }
+
+    [ForeignKey("IdEntrada")]
+    public int IdEntrada { get; set; }
+}
