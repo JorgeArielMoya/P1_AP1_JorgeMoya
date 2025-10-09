56f9888448efc147c05c02b3391a821e754556e1
jorge <jorge@LAPTOP-D8GDLQRL>
2025-10-08 21:23:23 -0400

    minuscula por mayuscula

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -5,7 +5,36 @@ namespace P1_AP1_JorgeMoya.DAL;
 
 public class Contexto : DbContext
 {
+    public DbSet<EntradasHuacalesDetalle> EntradasHuacalesDetalles { get; set; } 
     public DbSet<EntradasHuacales> EntradasHuacales { get; set; }
-
+    public DbSet<TiposHuacales> TiposHuacales { get; set; } 
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TiposHuacales>().HasData
+        (
+            new List<TiposHuacales>()
+            {
+                new ()
+                {
+                    TipoId = 1,
+                    Descripcion = "Rojo"
+                },
+
+                new ()
+                {
+                    TipoId = 2,
+                    Descripcion = "Verde"
+                },
+
+                new ()
+                {
+                    TipoId = 3,
+                    Descripcion = "Negro"
+                }
+            }
+        );
+        base.OnModelCreating(modelBuilder);
+    }
 }
