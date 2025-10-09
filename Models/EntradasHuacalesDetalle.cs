b5ed5c1e5c6ba0989e5879d6f43b2e6956226a34
jorge <jorge@LAPTOP-D8GDLQRL>
2025-10-08 21:12:30 -0400

    Se agrego la clase del detalle

---
+++ Models/EntradasHuacalesDetalle.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P1_AP1_JorgeMoya.Models;
+
+public class EntradasHuacalesDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+    public int IdEntrada { get; set; }
+    public int TipoId { get; set; }
+    public int Cantidad { get; set; }
+    public double Precio { get; set; }
+
+    [ForeignKey("IdEntrada")]
+    [InverseProperty("EntradasHuacalesDetalle")]
+    public virtual EntradasHuacales EntradaHuacal { get; set; }
+
+    [ForeignKey("TipoId")]
+    [InverseProperty("EntradasHuacalesDetalle")]
+    public virtual TiposHuacales TiposHuacales { get; set; } 
+}
